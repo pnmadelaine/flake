17be685616fbe791851d6843aa936504d1f5332e
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-29 00:15:39 +0200

    add example package

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,4 +1,7 @@
 {
   sources ? import ./sources.nix,
   system ? builtins.currentSystem or "unknown-system",
-}: {}
+  pkgs ? sources.nixpkgs.legacyPackages.${system},
+}: {
+  inherit (pkgs) hello;
+}
