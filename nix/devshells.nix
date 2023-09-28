29abf0e4ddbf912d1286ae144c61e2efacd39e80
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-28 18:14:09 +0200

    add example shell

--- nix/devshells.nix
+++ nix/devshells.nix
@@ -1,4 +1,14 @@
 {
   sources ? import ./sources.nix,
   system ? builtins.currentSystem or "unknown-system",
-}: {}
+  pkgs ? sources.nixpkgs.legacyPackages.${system},
+}: {
+  default = pkgs.mkShell {
+    packages = builtins.attrValues {
+      inherit
+        (pkgs)
+        alejandra
+        ;
+    };
+  };
+}
